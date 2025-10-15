dc7e51e7a36cb3e017269f639790178a20fb2155
Lucas Ferreira <luhvf.sfc@gmail.com>
2025-10-15 18:35:39 -0300

    Added the option to change the language between portuguese and english

--- JS/script.js
+++ JS/script.js
@@ -44,3 +44,40 @@ searchButton.addEventListener("click", ()=> {
     getWeather(searchBox.value);
 });
 
+const translations = {
+    pt: {
+        input: "Digite o nome da cidade",
+        error: "Nome da cidade inválido",
+        humidity: "Umidade",
+        speed: "Velocidade"
+    },
+    en: {
+        input: "Enter city name",
+        error: "Invalid city name",
+        humidity: "Humidity",
+        speed: "Speed"
+    }
+};
+
+const langSwitcher = document.getElementById("lang-switcher");
+
+const elements = {
+    input: document.getElementById("input-label"),
+    error: document.getElementById("error-label"),
+    humidity: document.getElementById("hum-label"),
+    speed: document.getElementById("speed-label")
+};
+
+function changeLanguage(lang) {
+    const t = translations[lang];
+    elements.input.placeholder = t.input;
+    elements.error.textContent = t.error;
+    elements.humidity.textContent = t.humidity;
+    elements.speed.textContent = t.speed;
+}
+
+langSwitcher.addEventListener("change", (e) => {
+    changeLanguage(e.target.value);
+});
+
+changeLanguage("pt");
